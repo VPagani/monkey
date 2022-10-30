644379c019d2d1492e37db5587c0880d4fdb8a5b
VPagani <xvpagani@gmail.com>
2022-10-30 01:04:27 -0300

    [monkey-go] Add function evaluation

--- src/monkey-go/object/object.go
+++ src/monkey-go/object/object.go
@@ -1,13 +1,19 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"monkey/ast"
+	"strings"
+)
 
 type ObjectType string
 
 const (
+	NULL_OBJ         ObjectType = "NULL"
 	INTEGER_OBJ      ObjectType = "INTEGER"
 	BOOLEAN_OBJ      ObjectType = "BOOLEAN"
-	NULL_OBJ         ObjectType = "NULL"
+	FUNCTION_OBJ     ObjectType = "FUNCTION"
 	RETURN_VALUE_OBJ ObjectType = "RETURN_VALUE"
 	ERROR_OBJ        ObjectType = "ERROR"
 )
@@ -16,6 +22,11 @@ type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+type Null struct{}
+
+func (obj *Null) Type() ObjectType { return NULL_OBJ }
+func (obj *Null) Inspect() string  { return "null" }
 
 type Integer struct {
 	Value int64
@@ -31,11 +42,6 @@ type Boolean struct {
 func (obj *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (obj *Boolean) Inspect() string  { return fmt.Sprintf("%t", obj.Value) }
 
-type Null struct{}
-
-func (obj *Null) Type() ObjectType { return NULL_OBJ }
-func (obj *Null) Inspect() string  { return "null" }
-
 type ReturnValue struct {
 	Value Object
 }
@@ -43,28 +49,35 @@ type ReturnValue struct {
 func (obj *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (obj *ReturnValue) Inspect() string  { return obj.Value.Inspect() }
 
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
+
 type Error struct {
 	Message string
 }
 
 func (obj *Error) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (obj *Error) Inspect() string  { return "ERROR: " + obj.Message }
-
-type Environment struct {
-	store map[string]Object
-}
-
-func NewEnvironment() *Environment {
-	s := make(map[string]Object)
-	return &Environment{store: s}
-}
-
-func (e *Environment) Get(name string) (Object, bool) {
-	obj, ok := e.store[name]
-	return obj, ok
-}
-
-func (e *Environment) Set(name string, value Object) Object {
-	e.store[name] = value
-	return value
-}
